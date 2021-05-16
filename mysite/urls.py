f3be47b701618ef761528733568819a7ad2e82f8
napestershine <napestershine@gmail.com>
2021-05-16 17:23:59 +0200

    fixed navigation

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('blog/', include('blog.urls'), name='blog-urls'),
     path('summernote/', include('django_summernote.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
-    path('admin/', admin.site.urls),
+    path('cms/admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
 
